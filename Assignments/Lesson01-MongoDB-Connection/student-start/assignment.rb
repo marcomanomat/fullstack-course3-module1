3c8c1680af828b01b7d6c30bc693e4c5a8ac75b0
Marco Manomat <marco.manomat@gmail.com>
2018-11-29 16:30:49 -0500

    finish module 1, assignment 1: MongoDB Ruby Driver Connection

--- Assignments/Lesson01-MongoDB-Connection/student-start/assignment.rb
+++ Assignments/Lesson01-MongoDB-Connection/student-start/assignment.rb
@@ -1,4 +1,10 @@
 require 'pp'
+require 'mongo'
+require 'byebug'
+require 'uri'
+
+Mongo::Logger.logger.level = ::Logger::INFO
+#Mongo::Logger.logger.level = ::Logger::DEBUG
 
 class Solution
   @@db = nil
@@ -8,27 +14,28 @@ class Solution
     #create a `Mongo::Client` connection to the server using a URL (.e.g., 'mongodb://localhost:27017')
     #configure the client to use the `test` database
     #assign the client to @@db class variable and return that client
-    
+    @@db = Mongo::Client.new('mongodb://localhost:27017/test')
   end
 
   #Implement a class method in the `Solution` class called `collection` that will
   def self.collection
     #return the `zips` collection
-    
+    self.mongo_client if not @@db
+    @@db[:zips]
   end
 
   #Implement an instance method in the `Solution` class called `sample` that will
   def sample
     #return a single document from the `zips` collection from the database. 
     #This does not have to be random. It can be first, last, or any other document in the collection.
-
+    self.class.collection.find.first
   end
 end
 
-#byebug
-db=Solution.mongo_client
-p db
-zips=Solution.collection
-p zips
-s=Solution.new
+# byebug
+db = Solution.mongo_client
+puts db
+zips = Solution.collection
+puts zips
+s = Solution.new
 pp s.sample
